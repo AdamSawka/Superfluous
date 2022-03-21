333c432c4c5e258d5a623ce0e22ee7c5d2741bd1
Adam Sawka <adam.sawka.net@gmail.com>
2022-03-21 15:02:43 +0100

    Created basic configuration

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const entryPath = '.';
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './' + path.join(entryPath, 'src/index.js'),
+  output: {
+    filename: 'out.js',
+    path: path.resolve(__dirname, path.join(entryPath, 'build')),
+    publicPath: '/build',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    static: {
+      directory: path.resolve(__dirname, entryPath),
+    },
+    compress: true,
+  }
+};
